b6b455a955d53d631bd350b700cb2e401e210a8e
Matty-88 <m94wing@gmail.com>
2025-05-12 03:32:49 -0700

    added storeFinder page/ drowdown selector component and map component

---
+++ src/app/(frontend)/storeFinder/page.tsx
@@ -0,0 +1,170 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import { Suspense, useState, useEffect, use } from 'react';
+import * as turf from '@turf/turf';
+
+
+const MapComponent = dynamic(() => import("@/components/Map/index"));
+const DropdownSelector = dynamic(() => import("@/components/DropdownSelector/index"), { ssr: false });
+
+export default function StoreFinderPage() {
+  const [selectedItem, setSelectedItem] = useState('');
+
+  const [locations, setLocations] = useState<any[]>([]);
+  const [searchStores, setSearchStores] = useState('');
+  const [userCoords, setUserCoords] = useState<[number, number] | null>(null);
+  const [filteredLocations, setFilteredLocations] = useState<any[]>([]);
+  const [selectedLocation, setSelectedLocation] = useState<any | null>(null);
+  const [kmRadius, setKmRadius] = useState(0);
+  const [locationList, setLocationList] = useState<any[]>([]);
+
+  console.log("locationList full:", locationList);
+
+
+
+  const handleSearchStores = async () => {
+    if (searchStores.length === 0) return;
+
+    const res = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(searchStores)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`);
+    const data = await res.json();
+
+    if (data.features.length > 0) {
+      const coords: [number, number] = [
+        data.features[0].geometry.coordinates[0],
+        data.features[0].geometry.coordinates[1],
+      ];
+
+      setUserCoords(coords);
+
+
+
+      const originPoint = turf.point(coords);
+
+
+      const nearby = await Promise.all(
+        locations.map(async (loc) => {
+          const address = [
+            loc.FINAL_NAME,
+            loc.Address_by_ID,
+            loc.City_by_ID,
+            loc.Province
+          ].join(', ');
+
+
+          const geores = await fetch(
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`
+          );
+          const j = await geores.json();
+          if (!j.features?.length) return null;
+
+
+          const [lng, lat] = j.features[0].geometry.coordinates;
+          const dest = turf.point([lng, lat]);
+          const dist = turf.distance(originPoint, dest, { units: 'kilometers' });
+
+
+          return dist <= kmRadius ? loc : null;
+        })
+      );
+
+      setFilteredLocations(nearby.filter((loc) => loc !== null));
+
+
+
+
+
+    } else {
+      console.log("No results found for the given location.");
+    }
+  };
+
+
+
+
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations?limit=1000`)
+      .then((res) => res.json())
+      .then((data) => {
+        const locations = data.docs;
+
+        setLocations(locations);
+      })
+      .catch((error) => {
+        console.error('Error. Could not fetch locations:', error);
+      });
+  }, []);
+
+
+  const listToDisplay = locationList.length > 0 ? locationList : filteredLocations.length > 0 ? filteredLocations : locations;
+
+
+
+
+
+  return (
+
+    <div className="flex h-screen">
+
+      <div className="w-1/3 bg-white shadow-lg p-4 overflow-y-auto text-black">
+        <div className="flex flex-col gap-4">
+          <DropdownSelector selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
+        </div>
+        <h1 className="text-2xl font-bold mb-4 text-gray-800">Find a Store</h1>
+
+        <input
+          type="text"
+          placeholder="Enter a location"
+          value={searchStores}
+          onChange={(e) => setSearchStores(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' && handleSearchStores()}
+          className="w-full p-2 border border-grey-300 rounded mb-4 text-black focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+        />
+
+        <input type="number"
+          placeholder='Set Radius'
+          value={kmRadius}
+          onChange={(e) => setKmRadius(Number(e.target.value))}
+          className="w-full p-2 border border-grey-300 rounded mb-4 text-black focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+        />
+
+
+
+        <button onClick={handleSearchStores} className="w-full bg-blue-500 text-white p-2 rounded mb-4 hover:bg-blue-600 transition duration-200">
+          Search
+        </button>
+
+        <ul className="space-y-3 text-black" >
+          {listToDisplay.map((location) => (
+            <li key={location.id} onClick={() => setSelectedLocation(location)} className="p-4 bg-gray-100 rounded shadow hover:bg-gray-200 transition duration-200">
+              <h2 className="text-lg font-semibold">{location.FINAL_NAME}</h2>
+              <p>{location.Address_by_ID}, {location.City_by_ID}, {location.Province}</p>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+
+
+      <div className="w-2/3 h-screen">
+        <div className="w-full h-full">
+          <MapComponent userCoords={userCoords} selectedItem={selectedItem} selectedLocation={selectedLocation} locationList={locationList} setLocationList={setLocationList} />        </div>
+      </div>
+
+    </div>
+
+
+
+
+
+
+
+  );
+}
+
+
+
+
+
+
---
+++ src/components/Map/index.tsx
@@ -0,0 +1,213 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import mapboxgl, { GeoJSONSource, Marker } from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import { stringify } from "qs-esm";
+import { Where } from "payload";
+
+
+// FILTERING FUNCTION, UNUESD FOR NOW
+// const getProducts = async (item: string) => {
+
+//   let formattedLocations: string[];
+//   let locationOBJ: any;
+
+//   const product_query: Where = {
+//     'product_id.product_name': {
+//       equals: item,
+//     },
+//   }
+
+//   let stringifiedQuery = stringify(
+//     {
+//       where: product_query,
+//     },
+//     { addQueryPrefix: true },
+//   )
+
+//   const product_response = await fetch(
+//     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/product_availability${stringifiedQuery}`,
+//   );
+
+//   const product_data = await product_response.json();
+//   locationOBJ = product_data.docs;
+
+//   formattedLocations = locationOBJ.map(
+//     (loc: any) => `${loc.location_id.FINAL_NAME}, ${loc.location_id.Address_by_ID}, ${loc.location_id.City_by_ID}, ${loc.location_id.Province}`
+//   );
+//   
+// }
+
+interface MapComponentProps {
+  userCoords: [number, number] | null;
+  selectedItem: string | null;
+  selectedLocation: any | null;
+  locationList: any | null;
+  setLocationList: any | null;
+}
+
+function plotPoints(locations: any, currentMap: any, markers: any) {
+  locations.forEach((loc: any) => {
+    let address: string = "";
+    let marker: Marker;
+
+    if (!loc.product_id) {
+      address = `${loc.FINAL_NAME}, ${loc.Address_by_ID}, ${loc.City_by_ID}, ${loc.Province}`;
+    } else {
+      address = `${loc.location_id.FINAL_NAME}, ${loc.location_id.Address_by_ID}, ${loc.location_id.City_by_ID}, ${loc.location_id.Province}`;
+    }
+
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapboxgl.accessToken}`)
+      .then(res => res.json())
+      .then(geo => {
+        if (!geo.features?.length) return;
+
+        const [lng, lat] = geo.features[0].geometry.coordinates;
+
+        if (!loc.product_id) {
+          marker = new mapboxgl.Marker()
+            .setLngLat([lng, lat])
+            .setPopup(new mapboxgl.Popup().setHTML(`
+                  <div style="color: black;">
+                    ${loc.FINAL_NAME}<br/>
+                    ${loc.Address_by_ID}<br/>
+                    ${loc.City_by_ID}, ${loc.Province}
+                  </div>
+                `))
+            .addTo(currentMap);
+        } else {
+          marker = new mapboxgl.Marker()
+            .setLngLat([lng, lat])
+            .setPopup(new mapboxgl.Popup().setHTML(`
+                  <div style="color: black;">
+                    ${loc.location_id.FINAL_NAME}<br/>
+                    ${loc.location_id.Address_by_ID}<br/>
+                    ${loc.location_id.City_by_ID}, ${loc.location_id.Province}
+                  </div>
+                `))
+            .addTo(currentMap);
+        }
+
+
+        markers.current.push(marker);
+      });
+  });
+}
+
+const MapComponent: React.FC<MapComponentProps> = ({ userCoords, selectedItem, selectedLocation, locationList, setLocationList }) => {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  let markers = useRef<mapboxgl.Marker[]>([]);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+    if (mapContainer.current) {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        center: [-123.1207, 49.2827],
+        zoom: 10,
+        maxZoom: 15,
+      });
+
+      mapRef.current = map;
+
+      const plotMarkers = async () => {
+        if (!selectedItem) return;
+
+        const query: Where = {
+          'product_id.product_name': { equals: selectedItem }
+        };
+
+        const stringifiedQuery = stringify({ where: query }, { addQueryPrefix: true });
+
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/product_availability${stringifiedQuery}`);
+        const data = await response.json();
+        const locations = data.docs;
+
+
+
+        //look here
+        setLocationList(locations.map((loc: any) => loc.location_id));
+
+
+
+
+        markers.current.forEach(marker => marker.remove());
+        plotPoints(locations, map, markers)
+      };
+
+      plotMarkers();
+
+      fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations?limit=1000`)
+        .then((res) => res.json())
+        .then((data) => {
+          markers.current.forEach(marker => marker.remove());
+          const locations = data.docs;
+          plotPoints(locations, map, markers)
+
+          //fullscreen 
+          map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body') }));
+
+          // Add zoom controls
+          map.addControl(new mapboxgl.NavigationControl(), "top-left");
+
+        });
+
+      // Clean up on unmount
+      return () => {
+        map.remove();
+        mapRef.current = null;
+      };
+    }
+  }, [selectedItem]);
+
+
+  useEffect(() => {
+    if (userCoords && mapRef.current) {
+      mapRef.current.flyTo({ center: userCoords, zoom: 12 });
+    }
+  }, [userCoords]);
+
+
+  useEffect(() => {
+    if (selectedLocation && mapRef.current) {
+      const address = `${selectedLocation.FINAL_NAME}, ${selectedLocation.Address_by_ID}, ${selectedLocation.City_by_ID}, ${selectedLocation.Province}`;
+
+      fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapboxgl.accessToken}`)
+        .then(res => res.json())
+        .then(geo => {
+
+          if (geo.features?.length) {
+            const [lng, lat] = geo.features[0].geometry.coordinates;
+            mapRef.current?.flyTo({
+              center: [lng, lat],
+              zoom: 14
+            });
+
+            const marker = markers.current.find(m => {
+              const markerLngLat = m.getLngLat();
+              return markerLngLat.lng === lng && markerLngLat.lat === lat;
+            });
+            marker?.togglePopup();
+          }
+        });
+    }
+  }, [selectedLocation]);
+
+
+  return (
+
+    <div
+      ref={mapContainer}
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    />
+  );
+};
+
+export default MapComponent;
