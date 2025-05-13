0820e59ba2fcee7e9d3edb80669f4c404b4e560c
Evin <evin4369@gmail.com>
2025-05-13 16:44:50 -0700

    Fixed my stuff being overwritten in other commit

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import mapboxgl, { GeoJSONSource, Marker } from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -43,93 +43,121 @@ function plotPoints(locations: any, currentMap: any, markers: any) {
   });
 }
 
-const MapComponent: React.FC<MapComponentProps> = ({ userCoords, setLocations, selectedItem, selectedLocation, products}) => {
+async function fetchAllLocations() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations?limit=2000`);
+  const data = await res.json();
+  return data.docs;
+}
+
+
+const MapComponent: React.FC<MapComponentProps> = ({ userCoords, setLocations, selectedItem, selectedLocation, products }) => {
+
+  // GLOBAL MAP COMPONENT VARIABLES
   const mapContainer = useRef<HTMLDivElement>(null);
   const mapRef = useRef<mapboxgl.Map | null>(null);
   let markers = useRef<mapboxgl.Marker[]>([]);
+  const [locationObj, setLocationObj] = useState<any[] | null>(null)
 
+  // STARTUP USE EFFECT, RUNS ONCE
   useEffect(() => {
-  mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
 
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const { latitude, longitude } = position.coords;
-      const map = new mapboxgl.Map({
-        container: mapContainer.current!,
-        center: [longitude, latitude],
-        zoom: 12,
-        maxZoom: 15,
-      });
-
-      mapRef.current = map;
-
-      fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations?limit=2000`)
-        .then(res => res.json())
-        .then(data => {
-          plotPoints(data.docs, map, markers);
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        const map = new mapboxgl.Map({
+          container: mapContainer.current!,
+          center: [longitude, latitude],
+          zoom: 12,
+          maxZoom: 15,
         });
 
-      map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body')! }));
-      map.addControl(new mapboxgl.NavigationControl(), "top-left");
-    },
-    (error) => {
-      console.error("Error getting location:", error);
-      const map = new mapboxgl.Map({
-        container: mapContainer.current!,
-        center: [-123.1207, 49.2827], 
-        zoom: 12,
-        maxZoom: 15,
-      });
+        mapRef.current = map;
 
-      mapRef.current = map;
+        // ONLY TIME DATABASE IS QUERIED
+        fetchAllLocations().then((locationsObj) => {
+          setLocationObj(locationsObj);
+          plotPoints(locationsObj, mapRef.current, markers);
 
-      fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations?limit=2000`)
-        .then(res => res.json())
-        .then(data => {
-          plotPoints(data.docs, map, markers);
+          map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body')! }));
+          map.addControl(new mapboxgl.NavigationControl(), "top-left");
+        });
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+
+        const map = new mapboxgl.Map({
+          container: mapContainer.current!,
+          center: [-123.1207, 49.2827],
+          zoom: 12,
+          maxZoom: 15,
         });
 
-      map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body')! }));
-      map.addControl(new mapboxgl.NavigationControl(), "top-left");
-    },
-    { enableHighAccuracy: true }
-  );
+        mapRef.current = map;
+
+        fetchAllLocations().then((locationsObj) => {
+          setLocationObj(locationsObj);
+          plotPoints(locationsObj, mapRef.current, markers);
+        });
+
+        map.addControl(new mapboxgl.FullscreenControl({ container: document.querySelector('body')! }));
+        map.addControl(new mapboxgl.NavigationControl(), "top-left");
+
+      },
+      {
+        enableHighAccuracy: true
+      }
+    );
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+        mapRef.current = null;
+      }
+    };
+  }, [setLocationObj]);
 
 
-  return () => {
-    if (mapRef.current) {
-      mapRef.current.remove();
-      mapRef.current = null;
-    }
-  };
-}, []);
 
-    
 
-      
 
-     useEffect(() => {
+
+
+  // ITEM FILTER USE EFFECT, ONLY RUNS WHEN ITEM FILTER IS USED
+  useEffect(() => {
     const plotFilteredMarkers = async () => {
-      if (!selectedItem || !mapRef.current) return;
-      
-      const selectedProduct = products.find((p: any) => p.productName === selectedItem );
-      
-     
+      if (!mapRef.current) return;
+
+      if(!selectedItem) {
+        plotPoints(locationObj, mapRef.current, markers);
+      }
+
+      const selectedProduct = products.find((p: any) => p.productName === selectedItem);
       const selectedProductId = selectedProduct?.id;
       if (!selectedProductId) return;
-      
+      // const query = new URLSearchParams({
+      //   'where[products][in]': selectedProductId, 'limit': '2000'
+      // }).toString();
 
-      const query = new URLSearchParams({
-        'where[products][in]': selectedProductId, 'limit': '2000'
-      }).toString();
+      // const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations?${query}`);
+      // const data = await response.json();
+      // const locations = data.docs;
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/locations?${query}`);
-      const data = await response.json();
-      const locations = data.docs;
+      //----
+      let locations: any[] = [];
+
+      locationObj?.forEach((location) => {
+        location.products.forEach((product: any) => {
+          if (product.id === selectedProductId) {
+            locations.push(location);
+          }
+        });
+      });
+      //----
 
       setLocations(locations);
 
-    
+
       markers.current.forEach(marker => marker.remove());
       markers.current = [];
 
@@ -139,17 +167,17 @@ const MapComponent: React.FC<MapComponentProps> = ({ userCoords, setLocations, s
     plotFilteredMarkers();
   }, [selectedItem, products]);
 
-
+// MOVES CAMERA WHEN SEARCHED BUTTON IS PRESSED
   useEffect(() => {
     if (userCoords && mapRef.current) {
       mapRef.current.flyTo({ center: userCoords, zoom: 12 });
     }
   }, [userCoords]);
 
-
+  // TRIGGERS WHEN A LOCATION IS SELECTED ON THE LEFT PANEL
   useEffect(() => {
     if (selectedLocation && mapRef.current) {
-  const address = `${selectedLocation.parentStore}, ${selectedLocation.address}, ${selectedLocation.city}, ${selectedLocation.province}`;
+      const address = `${selectedLocation.parentStore}, ${selectedLocation.address}, ${selectedLocation.city}, ${selectedLocation.province}`;
 
       fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${mapboxgl.accessToken}`)
         .then(res => res.json())
