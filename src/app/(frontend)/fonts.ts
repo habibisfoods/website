7bb4f58cab465bfe163f79abe797ed2baa59edfc
kvnbanunu <kvnbanunu@gmail.com>
2025-05-16 17:21:22 -0700

    minor styling and font

---
+++ src/app/(frontend)/fonts.ts
@@ -0,0 +1,6 @@
+import { Noto_Serif } from 'next/font/google'
+
+export const noto = Noto_Serif({
+  weight: ['500', '700'],
+  subsets: ['latin'],
+})
--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from 'next'
 import { cn } from '@/utilities/ui'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
+import { noto } from './fonts'
 import React from 'react'
-
 import { AdminBar } from '@/components/AdminBar'
 import { Footer } from '@/Footer/Component'
 import { Header } from '@/Header/Component'
@@ -20,7 +20,11 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   const { isEnabled } = await draftMode()
 
   return (
-    <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
+    <html
+      className={cn(noto.className, GeistSans.variable, GeistMono.variable)}
+      lang="en"
+      suppressHydrationWarning
+    >
       <head>
         <InitTheme />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
