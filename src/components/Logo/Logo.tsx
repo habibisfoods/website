612239e351705ab5cf19658e8aef6fc4055ed5c9
kvnbanunu <kvnbanunu@gmail.com>
2025-05-20 03:00:15 -0700

    fixed logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import React from 'react'
+import { getServerSideURL } from '@/utilities/getURL'
 
 interface Props {
   className?: string
@@ -13,6 +14,8 @@ export const Logo = (props: Props) => {
   const loading = loadingFromProps || 'lazy'
   const priority = priorityFromProps || 'low'
 
+  const imgURL = `${getServerSideURL()}/Habibis-Full-Logo.svg`
+
   return (
     /* eslint-disable @next/next/no-img-element */
     <img
@@ -24,7 +27,7 @@ export const Logo = (props: Props) => {
       decoding="async"
       className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
       // src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
-      src="Habibis-Full-Logo.svg"
+      src={imgURL}
     />
   )
 }
