bdae3936adb2ad7da861fca5e05b042ff7f5943b
kvnbanunu <kvnbanunu@gmail.com>
2025-05-20 02:53:36 -0700

    fixed import

--- src/app/(frontend)/storeFinder/layout.tsx
+++ src/app/(frontend)/storeFinder/layout.tsx
@@ -12,7 +12,7 @@ import { Providers } from '@/providers'
 import { InitTheme } from '@/providers/Theme/InitTheme'
 import { draftMode } from 'next/headers'
 
-import './globals.css'
+import '../globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
